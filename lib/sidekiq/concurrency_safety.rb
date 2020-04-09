720bd12f58d50259a747f8864adb26cb48589863
Tom Chipchase <tom.chipchase@farmdrop.co.uk>
2020-04-09 16:16:20 +0000

    Extract class and specs from project

--- lib/sidekiq/concurrency_safety.rb
+++ lib/sidekiq/concurrency_safety.rb
@@ -2,7 +2,42 @@ require "sidekiq/concurrency_safety/version"
 
 module Sidekiq
   module ConcurrencySafety
-    class Error < StandardError; end
-    # Your code goes here...
+    class AlreadyRunningError < StandardError; end
+
+    def perform(*args)
+      @args = args
+
+      redis_key = key_generator
+
+      redis_pool.with do |r|
+        raise AlreadyRunningError unless r.set(redis_key, 1, ex: 25_000, nx: true)
+
+        begin
+          super
+        ensure
+          r.del(redis_key)
+        end
+      end
+    end
+
+    private
+
+    attr_reader :args
+
+    def key_generator
+      ["job_running", self.class.name, final_key].join(":")
+    end
+
+    def final_key
+      concurrency_key_exists? ? concurrency_key : args.map(&:to_s)
+    end
+
+    def concurrency_key_exists?
+      methods.include?(:concurrency_key)
+    end
+
+    def redis_pool
+      Sidekiq.redis_pool
+    end
   end
 end
--- spec/sidekiq/concurrency_safety_spec.rb
+++ spec/sidekiq/concurrency_safety_spec.rb
@@ -1,9 +1,77 @@
+require "mock_redis"
+require "delegate"
+
 RSpec.describe Sidekiq::ConcurrencySafety do
-  it "has a version number" do
-    expect(Sidekiq::ConcurrencySafety::VERSION).not_to be nil
+  let(:job_class) do
+    Class.new(SimpleDelegator) do
+      prepend Sidekiq::ConcurrencySafety
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  subject { job_class.new(spy) }
+  let(:job_key) { "job_key" }
+  let(:key) { "job_running:#{job_class.name}" }
+  let(:redis) { MockRedis.new }
+
+  let(:connection_pool) { double :connection_pool }
+
+  before do
+    allow(connection_pool).to receive(:with).and_yield(redis)
+    allow(subject).to receive(:redis_pool).and_return connection_pool
+  end
+
+  describe "#perform" do
+    context "when the key has not been set" do
+      let(:spy) { instance_spy "spy" }
+
+      it "runs job" do
+        subject.perform
+        expect(spy).to have_received(:perform)
+      end
+    end
+
+    context "after a failure" do
+      let(:spy) { instance_spy "spy" }
+
+      before do
+        allow(spy).to receive(:perform).and_raise RuntimeError
+        expect { subject.perform }.to raise_error(RuntimeError)
+        allow(spy).to receive(:perform).and_return true
+      end
+
+      it "can be queued up again" do
+        expect { subject.perform }.not_to raise_error
+      end
+    end
+
+    context "when two instances try to run" do
+      let(:spy) { instance_spy "spy", perform: true }
+
+      before do
+        allow(spy).to receive(:perform) { sleep 10 }
+      end
+
+      it "does not run job" do
+        Thread.new { subject.perform }
+        sleep 1
+        expect { subject.perform }
+          .to raise_error described_class::AlreadyRunningError
+      end
+    end
+
+    context "when run with different arguments" do
+      let(:spy) { instance_spy "spy" }
+
+      before do
+        allow(spy).to receive(:perform).with(:foo) { sleep 10 }
+        allow(spy).to receive(:perform).with(:bar)
+      end
+
+      it "does not run job" do
+        Thread.new { subject.perform(:foo) }
+        sleep 1
+        expect { subject.perform(:bar) }.not_to raise_error
+      end
+    end
   end
 end
